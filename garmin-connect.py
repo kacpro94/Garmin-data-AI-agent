bb49cb8db6ef144669d53ae8235d0ec14e040385
Kacper Prorok <kacpro944@gmail.com>
2026-05-23 20:31:48 +0200

    change time sleep

--- garmin-connect.py
+++ garmin-connect.py
@@ -103,7 +103,7 @@ def synchronizuj_garmin_do_sheets():
                 
                 # Dopisujemy okrążenie na koniec karty Laps
                 laps_sheet.append_row([akt_id, numer_lap, dystans_km, wynik_ruchu, tetno_int, kadencja_int])
-                time.sleep(1.2)
+                time.sleep(3)
                 
         print("Synchronizacja zakończona sukcesem!")
         
